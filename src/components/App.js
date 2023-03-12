9ad6344706cb52adcea2e61aebbc177f6ca8fcaa
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-03-12 14:18:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,46 @@ import '../styles/App.css';
 const App = () => {
 
 //code here 
+  const emailRef =useRef();
+  const passwordRef =useRef();
+  const[formInput,setFormInput]=useState({email:"",
+                                          password:"",});
+  const[displayMsg,setDisplayMsg]=useState({ emailMsg:"",passwordMsg:"",});
+  const handleInputeChange =(e)=>{
+    e.persist();
+    e.target.id,e.target.value);
+    if(e.target.value);
+    if(e.traget.id ==="inputEmail"){
+      setFormaInput((state)=>{
+        return{
+          ...state,["email"]:e.target.value,};
+      });
+    }else{
+      setFormInput((state)=>{
+        return{...state,["password"]:
+               e.target.value,};
+      });
+    }
+  };
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    if(formInput.email===")
+       
+       {
+       emailRef.current.focus();
+    setDisplayMsg({
+      emailMs:"",
+      passwordMsg:"",
+    });
+  }
+  else{
+    setDisplayMsg({
+      emailMsg:formaInput.email,passwordMsg:formInput.password,});
+    setFormInput({
+      email:"",password"",});
+  }
+};             
+                                          
 
   return (
     <div id="main">
@@ -11,8 +51,8 @@ const App = () => {
       Password
       <input id="inputPassword" type="text" value={} ref={} onChange={}/><br/>
       <button id="submitButton" onClick={}>Submit</button><br/>
-      <p id="emailText">Your Email : {}</p>
-      <p id ="passwordText">Your Password : {}</p>
+      <p id="emailText">Your Email : {displayMsg.emailMsg}</p>
+      <p id ="passwordText">Your Password : {displayMsg.passwordMsg}</p>
       
     </div>
   )
